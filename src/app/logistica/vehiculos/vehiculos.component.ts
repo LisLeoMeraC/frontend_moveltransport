324cc5140b114c28529c8209a98144d698de78ff
LisleoMeraC <meraleonardo99@gmail.com>
2025-05-02 16:58:39 -0500

    Add diseño Vehiculo

--- src/app/logistica/vehiculos/vehiculos.component.ts
+++ src/app/logistica/vehiculos/vehiculos.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
@@ -12,7 +13,7 @@ import { ToolbarModule } from 'primeng/toolbar';
   selector: 'app-vehiculos',
   standalone: true,
   imports: [CommonModule,ToolbarModule,TableModule,InputTextModule,IconFieldModule,InputIconModule,ButtonModule,
-    DialogModule
+    DialogModule, DropdownModule
   ],
   templateUrl: './vehiculos.component.html',
   styleUrl: './vehiculos.component.scss'
@@ -29,6 +30,8 @@ export class VehiculosComponent {
   closeDialogVehicle(){
     this.dialogVehicle=false; 
   }
+
+  
 
 
 
